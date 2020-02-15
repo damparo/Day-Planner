1d8d8b3c6bca3a52b413cb08effe949404d5471c
Dennis Amparo <dennisamparo11@gmail.com>
2020-02-14 19:42:35 -0700

    preFinal

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Day-Planner
+
+## File
+
+*['Day-Planner'] https://damparo.github.io/Day-Planner/
+
+* The goal of this assignment was to create a workday scheduler that would allow the user to save their to-do's.
+
+* The scheduler relies on local storage to display past to-do's.  Another feature is green-red alert notice that shows future time as green, and current time as red for better to-do management.
+
+* jquery was used to create the dynamic buttons that would allow to save info,and also recall if from local storage.
+
+* Unfortunately, my local storage code is not able to retrieve information at the moment.  I will sort this out to make it function.
+
---
+++ javascript.js
@@ -0,0 +1,75 @@
+
+$(".hour").each(function() {
+    $(this)
+      .removeClass("present")
+      .removeClass("future")
+      .removeClass("past");
+
+    var currentHour = moment().format("h");
+    var dataNum = $(this).attr("data-num");
+    // console.log(dataNum);
+    // console.log(currentHour);
+
+    if (dataNum < currentHour) {
+      $(this).addClass("past");
+    }
+    if (dataNum === currentHour) {
+      $(this).addClass("present");
+    }
+
+    if (dataNum > currentHour) {
+      $(this).addClass("future");
+    }
+  });
+  
+  var currentDay = $("#currentDay");
+  var interval = setInterval(SetTime, 1000);
+
+  function SetTime() {
+  var date = moment().format("dddd, MMMM Do YYYY, k:mm:ss");
+  currentDay.innerHTML = date;
+  console.log(date);
+  $("#currentDay").text(date);
+  };
+
+
+  //Local Storage will not work - will come back for this
+  //something to do with retrieving my infor
+
+  // var notes = getNotes();
+  // renderNotes();
+
+  // $(".saveBtn").on("click", function(e) {
+  // e.preventDefault();
+  // var toDoList = $(this)
+  //   .siblings("col-8")
+  //   .val();
+  // var hour = $(this)
+  //   .siblings("col-8")
+  //   .attr("id");
+  // notes[hour] = toDoList;
+  // // console.log(toDoList);
+
+  // localStorage.setItem("notes", JSON.stringify(notes));
+  // });
+
+  
+
+  // function getNotes() {
+  //   var notes = localStorage.getItem("notes");
+  //   if (notes) {
+  //     notes = JSON.parse(notes);
+  //   } else {
+  //     notes = {};
+  //   }
+  //   return notes;
+  // }
+
+
+  // function renderNotes (){
+  //   for ( var key in notes) {
+  //     $("#" + key).val(notes[key]);
+  //   }
+  // }
+
+//retrieve items from local storage

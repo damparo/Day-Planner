ad50af09043866744b754697399c14a18de3522f
Dennis Amparo <dennisamparo11@gmail.com>
2020-03-24 18:29:40 -0700

    Functional

--- README.md
+++ README.md
@@ -1,14 +1,21 @@
 # Day-Planner
 
-## File
+## Project Goal
 
-*['Day-Planner'] https://damparo.github.io/Day-Planner/
+The goal of this assignment was to create a workday scheduler that would allow the user to save their to-do's.
 
-* The goal of this assignment was to create a workday scheduler that would allow the user to save their to-do's.
+## Functionality
 
-* The scheduler relies on local storage to display past to-do's.  Another feature is green-red alert notice that shows future time as green, and current time as red for better to-do management.
+The scheduler relies on local storage to display past to-do's.  Another feature is green-red alert notice that shows future time as green, and current time as red for better to-do management.
 
-* jquery was used to create the dynamic buttons that would allow to save info,and also recall if from local storage.
+jquery was used to create the dynamic buttons that would allow to save info, and also recall it from local storage.
 
-* Unfortunately, my local storage code is not able to retrieve information at the moment.  I will sort this out to make it function.
+Project link [here](https://damparo.github.io/Day-Planner/)
 
+## Thoughts:
+
+Local storage is functional.
+Timeblocks are color coded.
+
+## Update:
+No further updates for functionality.
--- javascript.js
+++ javascript.js
@@ -28,48 +28,47 @@ $(".hour").each(function() {
   function SetTime() {
   var date = moment().format("dddd, MMMM Do YYYY, k:mm:ss");
   currentDay.innerHTML = date;
-  console.log(date);
+  // console.log(date);
   $("#currentDay").text(date);
   };
 
 
-  //Local Storage will not work - will come back for this
-  //something to do with retrieving my infor
+  var notes = getNotes();
+  renderNotes();
 
-  // var notes = getNotes();
-  // renderNotes();
+  $(".saveBtn").on("click", function(e) {
+  e.preventDefault();
 
-  // $(".saveBtn").on("click", function(e) {
-  // e.preventDefault();
-  // var toDoList = $(this)
-  //   .siblings("col-8")
-  //   .val();
-  // var hour = $(this)
-  //   .siblings("col-8")
-  //   .attr("id");
-  // notes[hour] = toDoList;
-  // // console.log(toDoList);
 
-  // localStorage.setItem("notes", JSON.stringify(notes));
-  // });
+  var toDoList = $(this)
+  .siblings(".col-8")
+  .val();
+  
+
+  var hour = $(this)
+    .siblings(".col-8")
+    .attr("id");
+  notes[hour] = toDoList;
+  // console.log(toDoList);
+
+  localStorage.setItem("notes", JSON.stringify(notes));
+  });
 
   
 
-  // function getNotes() {
-  //   var notes = localStorage.getItem("notes");
-  //   if (notes) {
-  //     notes = JSON.parse(notes);
-  //   } else {
-  //     notes = {};
-  //   }
-  //   return notes;
-  // }
+  function getNotes() {
+    var notes = localStorage.getItem("notes");
+    if (notes) {
+      notes = JSON.parse(notes);
+    } else {
+      notes = {};
+    }
+    return notes;
+  }
 
 
-  // function renderNotes (){
-  //   for ( var key in notes) {
-  //     $("#" + key).val(notes[key]);
-  //   }
-  // }
-
-//retrieve items from local storage
+  function renderNotes (){
+    for ( var key in notes) {
+      $("#" + key).val(notes[key]);
+    }
+  }
